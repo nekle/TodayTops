bcd0e3906e53fa7be30cb515165782977d3dc105
nekle <806337948@qq.com>
2021-11-14 15:38:00 +0800

    格式改动

--- components/newsContent.vue
+++ components/newsContent.vue
@@ -1,10 +1,8 @@
 <template>
 	<view>
-		{{tabName}}
 		<!-- scroll-left="120"；右边的元素上来的距离 -->
 		<scroll-view class="scroll-view_V" scroll-y="true" show-scroll-bar=true>
 			<view class="scroll-view-item_V" v-for="(item,index) in data" :key="index">
-				{{index}}
 				<textNewsPreview :title="item.title" :author="item.author" :status="item.status" :date="item.date">
 				</textNewsPreview>
 			</view>
@@ -41,18 +39,25 @@
 </script>
 
 <style>
+	* {
+		text-decoration: none;
+		box-sizing: border-box;
+		margin: 0;
+		padding: 0;
+	}
+
 	.scroll-view_V {
 		/* 文本不会换行，文本会在在同一行上继续，直到遇到 <br> 标签为止。 */
 		white-space: nowrap;
 		width: 100%;
 		height: 900upx;
+		margin-left: 2%;
 	}
 
 	.scroll-view-item_V {
 		display: block;
 		width: 80%;
 		height: 140upx;
-		border: 1upx solid;
 		text-align: center;
 		font-size: 36upx;
 		margin: 2% 1% 2% 1%;
@@ -61,5 +66,4 @@
 	.scroll-view-item_V view {
 		box-sizing: border-box;
 	}
-
 </style>
